69183231c6330d37d28fe3c97009ba786fa6f8b5
Tyler Drury <c0661137@mylambton.ca>
2017-04-11 18:50:07 -0400

    added dbutils class and connected to a database.

---
+++ src/java/Classes/DBUtils.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author c0661137
+ */
+public class DBUtils {
+
+    private final static String studentNumber = "c0661137";
+
+    /**
+     * Utility method used to create a Database Connection
+     *
+     * @return the Connection object
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+
+        }
+        String server = "ipro.lambton.on.ca";
+        String username = studentNumber + "-java";
+        String password = studentNumber;
+        String database = username;
+        String jdbc = String.format("jdbc:mysql://%s/%s", server, database);
+        return DriverManager.getConnection(jdbc, username, password);
+    }
+}
