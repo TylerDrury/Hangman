e2048c5b2e2bb50c15b1520c5915aec6de01a661
Tyler Drury <c0661137@mylambton.ca>
2017-04-11 21:46:41 -0400

    added an index page and made it so that all words will be displayed from the database onto a table for testing purposes.

--- src/java/Classes/wordController.java
+++ src/java/Classes/wordController.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,32 +24,51 @@ import javax.inject.Named;
 @Named
 @ApplicationScoped 
 public class wordController {
-    private word gameWord = new word();
+    private word thisWord = new word();
+    private List<word> wordList = new ArrayList<>();
     
+     public wordController() {
+        thisWord = new word("", 0);
+        getPostsFromDB();
+    }
       private void getPostsFromDB() {
         try (Connection conn = DBUtils.getConnection()) {       
             Statement stmt = conn.createStatement();
             Random rnd = new Random();
             int rand = rnd.nextInt(29)+1;
-            ResultSet rs = stmt.executeQuery("SELECT * FROM posts WHERE wordId = " + rand);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM words");
             while (rs.next()) {
-                gameWord = new word(
-                        rs.getString("title"),
-                        rs.getInt("wordId")
-                );
-               
+               word w = new word();
+               w.setWord(rs.getString("title"));
+               w.setWordId(rs.getInt("wordId"));
+               wordList.add(w);
             }
         } catch (SQLException ex) {
             Logger.getLogger(wordController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public word getGameWord() {
-        return gameWord;
+    public word getThisWord() {
+        return thisWord;
     }
 
-    public void setGameWord(word gameWord) {
-        this.gameWord = gameWord;
+    public void setThisWord(word thisWord) {
+        this.thisWord = thisWord;
     }
+
+    public List<word> getWordList() {
+        return wordList;
+    }
+
+   public word getByWordId(int id) {
+        for (word i : wordList) {
+            if (i.getWordId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    
       
 }
